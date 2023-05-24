1aa064e6c68aba00670a7f25135ce3b88001d325
Kate-Karui <>
2023-05-24 04:49:54 +0000

    add text entry and score count

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 let questions = [
-    'Who is the main antagonist of The Legend Of Zelda series?',
+    "Who is the main antagonist of The Legend Of Zelda series?",
     "Who's the last character to be added in Super Smash Brothers Ultimate?",
-    'Where are you from?',
-    'What is your favorite color?',
-    'What are your hobbies?'
+    "Where are you from?",
+    "What is your favorite color?",
+    "What are your hobbies?"
 ];
+
+
+let answers = [
+    "Ganon",
+    "Sora",
+    "Your country name",
+    "Your color name",
+    "Your hobbies"
+];
+
+let score = 0;
 
 function getRandomQuestion() {
     let index = Math.floor(Math.random() * questions.length);
@@ -17,10 +28,38 @@ function questionArea() {
     question = getRandomQuestion();
 
     document.getElementsByTagName('h1')[0].innerHTML = question;
-    console.log('Hi!');
+    console.log('Test!');
 
 }
 
 
 
 questionArea();
+
+
+const quizContainer = document.getElementById('questionarea');
+const resultsContainer = document.getElementById('scorearea');
+const submitButton = document.getElementById('submit');
+const inputElement = document.getElementById('input');
+const scoreElement = document.getElementById('score');
+
+function checkAnswer() {
+    let userAnswer = inputElement.value;
+    let correctAnswer = answers[questions.indexOf(question)];
+    if (userAnswer === correctAnswer) {
+        score++;
+        alert("Correct!");
+    } else {
+        score--;
+        alert("Wrong!");
+    }
+    questionArea();
+    inputElement.value = "";
+    scoreElement.innerHTML = score;
+}
+submitButton.addEventListener("click", checkAnswer);
+
+function updateGameArea() {
+    scoreText.text = "SCORE: " + score;
+    scoreText.update();
+}
