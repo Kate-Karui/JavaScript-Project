eb82f3801f1b443cd045d28cf59f75141613e555
Kate-Karui <katekarui601@gmail.com>
2023-10-02 12:25:07 +0200

    revamp question to 3 choice, add some detail on comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-let questions = [
-    "Who is the main antagonist in The Legend Of Zelda series?",
-    "Who's the last character to be added in Super Smash Brothers Ultimate?",
-    "What is the game engine made for Half Life 2 called?",
-    "What's the highest grossing game?",
-    "What's Mario's first apperance?",
+/**
+ * Question array matching the answers array
+ */
+const questions = [
+	"Who is the main antagonist in The Legend Of Zelda series?",
+	"Who's the last character to be added in Super Smash Brothers Ultimate?",
+	"What is the game engine made for Half Life 2 called?",
+	"What's the highest grossing game?",
+	"What's Mario's first apperance?",
 	"Nintendo began as a company selling which products?",
 	"What is the name of the alien race that invades Earth in Halo?",
 	"What game is Little Mac from?",
@@ -38,131 +41,203 @@ let questions = [
 	"What's the starting location in Jet Set Radio?",
 	"In the first Five Night's At Freddy's game, what song gets played when you run out of power?",
 	"In Yakuza, who teaches Kiryu his famous Tiger Drop technique?"
-	
 ];
 
-
-let answers = [
-    "Ganon",
-    "Sora",
-    "Source",
-    "Minecraft",
-    "Donkey Kong",
-	"Playing Cards",
-	"Covenant",
-	"Punch Out",
-	"Hyrule",
-	"Sephiroth",
-	"Dead Space",
-	"Lara Croft",
-	"Sims",
-	"Witcher",
-	"Borderlands",
-	"Kratos",
-	"Roblox",
-	"Nathan Drake",
-	"Steam",
-	"Bethesda",
-	"Pink",
-	"Pong",
-	"World of Warcraft",
-	"Jump Man",
-	"Microsoft",
-	"Rad Mobile",
-	"Architecture Designs",
-	"Temple Of Time",
-	"Tom Nook",
-	"Flash Liquidising Ultra Dousing Device",
-	"A Hat In Time",
-	"Sunflower Orphanage",
-	"Modding",
-	"Mouse",
-	"Tower Defense",
-	"Jiro",
-	"Shibuya",
-	"Toreador March",
-	"Sotaro Komaki"
-	
+const answers = [
+	"Ganondorf, Zant, Demise",
+	"Sora, Steve, Pyra & Mythra",
+	"Source, Frostbite, Unreal Engine",
+	"Minecraft, Fortnite, Tetris",
+	"Donkey Kong, Super Mario Bros, Mario Kart",
+	"Playing Cards, Books, Chess",
+	"Covenant, Flood, Prometheans",
+	"Punch Out, Kid Icarus, Worldrunner",
+	"Hyrule, Termina, Koholint",
+	"Sephiroth, Kefka, Kuja",
+	"Dead Space, Resident Evil, Silent Hill",
+	"Lara Croft, Nathan Drake, Ezio Auditore",
+	"Sims, Sime, Simo",
+	"Witcher, Dragon Age, Mass Effect",
+	"Borderlands, Destiny, Warframe",
+	"Kratos, Hires, Zeus",
+	"Roblox, Minecraft, Terraria",
+	"Nathan Drake, Gerald Gardener, Booker DeWitt",
+	"Steam, GOG, Epic Games Store",
+	"Bethesda, Bioware, CD Projekt Red",
+	"Pink, Green, Blue",
+	"Pong, Space Invaders, Pac-Man",
+	"World of Warcraft, Everquest, Runescape",
+	"JumpMan, RunMan, Moustache",
+	"Microsoft, Apple, IBM",
+	"Rad Mobile, Pole Position, Out Run",
+	"Architecture Designs, Graphic Design, Interior Design",
+	"Temple Of Time, Temple Of Light, Temple Of Courage",
+	"Tom Nook, Isabelle, K.K. Slider",
+	"Flash Liquidising Ultra Dousing Device, Flowing Liquidising Ultimate Destruction Device, Flying Liquid Unleashing Drenching Device",
+	"A Hat In Time, Celeste, Hollow Knight",
+	"Sunflower Orphanage, Morning Glory Orphanage, Daisy Orphanage",
+	"Modding, Speedrunning, Famous Creators",
+	"Mouse, Rat, Beaver",
+	"Tower Defense, RPG, Turn-Based Strategy",
+	"Jiro, Hoxton, Chains",
+	"Shibuya, Akihabara, Kyoto",
+	"Toreador March, Pop Goes The Weasel, London Bridge Is Falling Down",
+	"Sotaro Komaki, Kora Bishi, Masaru Sera"
 ];
-
 
 let score = 0;
 let prevQuestionIndex = -1;
+let remainingQuestions = questions.slice();
+
 function getRandomQuestion() {
-    let index = Math.floor(Math.random() * questions.length);
-    while (index === prevQuestionIndex) {
-        index = Math.floor(Math.random() * questions.length);
-    }
-    prevQuestionIndex = index;
-    return questions[index];
+	if (remainingQuestions.length === 0) {
+		remainingQuestions = questions.slice();
+	}
+	let index = Math.floor(Math.random() * remainingQuestions.length);
+	let question = remainingQuestions[index];
+	remainingQuestions.splice(index, 1);
+	return question;
 }
-
-
+/**
+ * Shuffles the contents of the answers array
+ * @param {string} array 
+ */
+function shuffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}
 
 function questionArea() {
-
-    question = getRandomQuestion();
-
-    document.getElementsByTagName('h1')[0].innerHTML = question;
+	let question = getRandomQuestion();
+	document.getElementsByTagName('h1')[0].innerHTML = question;
+	let splitAnswers = answers[questions.indexOf(question)].split(',');
+	shuffle(splitAnswers);
+	let correctAnswer = splitAnswers[0];
+	document.getElementById('choice1').value = correctAnswer;
+	document.getElementById('choice2').value = splitAnswers[1];
+	document.getElementById('choice3').value = splitAnswers[2];
 }
+
+function checkGameStatus() {
+	if (score == 10) {
+		alert("You win!");
+		location.reload();
+	}
+	if (score == 0) {
+		alert("You lose!");
+		location.reload();
+	}
+}
+/**
+ * Checks if the user's answer is correct or not
+ * @param {string} selectedChoice 
+ */
+function checkAnswer(selectedChoice) {
+	let userAnswer = selectedChoice.value.toLowerCase();
+	let correctAnswer = answers[questions.indexOf(document.getElementsByTagName('h1')[0].innerHTML)].toLowerCase().split(',')[0];
+	if (userAnswer === correctAnswer) {
+		score++;
+		showImage("check.jpg", "Correct!");
+	} else {
+		if (score > 0) {
+			score--;
+		}
+		showImage("cross.jpg", "Wrong!");
+	}
+	checkGameStatus();
+	document.getElementById("score").innerHTML = "Score: " + score;
+	setTimeout(questionArea, 1000);
+}
+
 
 function showImage(imageSrc, altText) {
 	var img = document.createElement("img");
-	img.src = imageSrc;
+	img.src = "assets/images/" + imageSrc;
 	img.alt = altText;
 	var src = document.getElementById("header");
-    src.appendChild(img);
+	src.appendChild(img);
+	setTimeout(function () {
+		document.getElementById('choice0').addEventListener('click', function () {
+			checkAnswer(document.getElementById(`choice${Math.floor(Math.random() * 3) + 1}`));
+		});
+	}, 1000);
 }
 
 questionArea();
-const submitButton = document.getElementById('submit');
-const inputElement = document.getElementById('input');
+const choice1 = document.getElementById('choice1');
+const choice2 = document.getElementById('choice2');
+const choice3 = document.getElementById('choice3');
 const scoreElement = document.getElementById('score');
 
-function checkGameStatus() {
-  if (score == 10) {
-    alert("You win!");
-    location.reload();
-  }
-  if (score == 0) {
-    alert("You lose!");
-    location.reload();
-  }
-}
-
-
-function checkAnswer() {
-    let userAnswer = inputElement.value.toLowerCase();
-    let correctAnswer = answers[questions.indexOf(question)].toLowerCase();
-    if (userAnswer.includes(correctAnswer)) {
- showImage("assets/images/check.jpg", "Checkmark");
-		score++;
-		answers.splice(questions.indexOf(question), 1);
-		questions.splice(questions.indexOf(question), 1);
-        alert("Correct!");
-    } else {
-        if (score > 0) {
-            score--;
-        }
-		showImage("assets/images/cross.jpg", "Cross mark");
-		answers.splice(questions.indexOf(question), 1);
-		questions.splice(questions.indexOf(question), 1);
-        let capitalizedAnswer = correctAnswer.charAt(0).toUpperCase() + correctAnswer.slice(1);
-        alert("Wrong! The correct answer is " + capitalizedAnswer);
-    }
-    questionArea();
-    inputElement.value = "";
-    scoreElement.innerHTML = score;
-  checkGameStatus();
-
-}
-
-inputElement.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        checkAnswer();
-    }
+choice1.addEventListener('click', function () {
+	checkAnswer(choice1);
 });
-submitButton.addEventListener("click", checkAnswer);
+
+choice2.addEventListener('click', function () {
+	checkAnswer(choice2);
+});
+
+choice3.addEventListener('click', function () {
+	checkAnswer(choice3);
+});
+
+document.getElementById('choice0').addEventListener('click', function () {
+	checkAnswer(document.getElementById(`choice${Math.floor(Math.random() * 3) + 1}`));
+});
+
+document.getElementById('submit').addEventListener('click', function () {
+	let userAnswer = document.getElementById('input').value.toLowerCase();
+	let correctAnswer = answers[questions.indexOf(document.getElementsByTagName('h1')[0].innerHTML)].toLowerCase().split(',')[0];
+	if (userAnswer === correctAnswer) {
+		score++;
+		showImage("check.jpg", "Correct!");
+		setTimeout(function () {
+			choice1.addEventListener('click', function () {
+				checkAnswer(choice1);
+			});
+
+			choice2.addEventListener('click', function () {
+				checkAnswer(choice2);
+			});
+
+			choice3.addEventListener('click', function () {
+				checkAnswer(choice3);
+			});
+
+			document.getElementById('choice0').addEventListener('click', function () {
+				checkAnswer(document.getElementById(`choice${Math.floor(Math.random() * 3) + 1}`));
+			});
+		}, 1000);
+	} else {
+		if (score > 0) {
+			score--;
+		}
+		showImage("cross.jpg", "Wrong!");
+		setTimeout(function () {
+			checkGameStatus();
+			questionArea();
+			choice1.addEventListener('click', function () {
+				checkAnswer(choice1);
+			});
+
+			choice2.addEventListener('click', function () {
+				checkAnswer(choice2);
+			});
+
+			choice3.addEventListener('click', function () {
+				checkAnswer(choice3);
+			});
+
+			document.getElementById('choice0').addEventListener('click', function () {
+				checkAnswer(document.getElementById(`choice${Math.floor(Math.random() * 3) + 1}`));
+			});
+		}, 1000);
+	}
+	document.getElementById("score").innerHTML = "Score: " + score;
+});
+
+document.getElementById("score").innerHTML = "Score: " + score;
 
 
-inputElement.focus();
